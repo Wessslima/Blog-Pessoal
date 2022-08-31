2031488e182ed0ff20ed6c4c2e1788c42d4d7b9c
unknown <wes_lima@outlook.com>
2022-08-31 20:11:04 -0300

    Adicionando o Método de busca específica e CRUD

---
+++ blogpessoal-2/repository/PostagemRepository.java
@@ -0,0 +1,19 @@
+package org.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.generation.blogpessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>  {
+	
+	//SELECT * FROM tb_postagens WHERE titulo LIKE "%%";
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+	//SELECT * FROM tb_postagens WHERE texto LIKE "%%";
+	
+		public List <Postagem> findAllByTextoContainingIgnoreCase(String titulo);
+}
